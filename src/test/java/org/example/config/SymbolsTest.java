c5babbc184989eddaedba582dce007ae721b4898
mprzybylski <martin.p2907@hotmail.com>
2024-03-08 12:29:49 +0000

    Update tests

--- src/test/java/org/example/config/SymbolsTest.java
+++ src/test/java/org/example/config/SymbolsTest.java
@@ -1,13 +1,12 @@
 package org.example.config;
 
-import org.example.config.Symbols;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
--- src/test/java/org/example/factory/StrategyFactoryTest.java
+++ src/test/java/org/example/factory/StrategyFactoryTest.java
@@ -1,18 +1,22 @@
-package org.example.strategy;
+package org.example.factory;
 
-import org.example.strategy.implementation.StrandardStrategy;
+import org.example.strategy.Strategy;
+import org.example.strategy.implementation.StandardStrategy;
 import org.example.symbol.Symbol;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StrategyFactoryTest {
 
     @Autowired
@@ -20,18 +24,16 @@ class StrategyFactoryTest {
 
     @Test
     void givenStrategyNameExists_whenCreateStrategyIsInvoked_thenNewInstanceIsCreated() {
-        final Symbol mockSymbol = mock(Symbol.class);
-        when(mockSymbol.getStrategyName()).thenReturn("StandardStrategy");
-
-        final Strategy strategy = strategyFactory.createStrategy(mockSymbol);
+        final Symbol symbol = new Symbol("Test", "StandardStrategy", null);
+        final Strategy strategy = strategyFactory.createStrategy(symbol);
 
         assertThat(strategy).isNotNull();
-        assertThat(strategy).isInstanceOf(StrandardStrategy.class);
+        assertThat(strategy.getStrategyName()).isEqualTo("StandardStrategy");
 
-        final Strategy strategy2 = strategyFactory.createStrategy(mockSymbol);
+        final Strategy strategy2 = strategyFactory.createStrategy(symbol);
 
         assertThat(strategy).isNotNull();
-        assertThat(strategy).isInstanceOf(StrandardStrategy.class);
+        assertThat(strategy.getStrategyName()).isEqualTo("StandardStrategy");
 
         assertThat(strategy).isNotEqualTo(strategy2);
     }
