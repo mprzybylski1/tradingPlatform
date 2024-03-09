c5eb71c4d2f98ad356bb3749aa511395199325d5
mprzybylski <martin.p2907@hotmail.com>
2024-03-09 15:42:39 +0000

    Add historic data queue

--- src/main/java/org/example/strategy/BaseStrategy.java
+++ src/main/java/org/example/strategy/BaseStrategy.java
@@ -1,12 +1,14 @@
 package org.example.strategy;
 
 import lombok.extern.log4j.Log4j2;
-import org.example.enums.Timeframe;
 import org.example.poller.DataPoller;
 import org.example.valueobject.Candle;
 import org.example.valueobject.Symbol;
 import org.springframework.scheduling.annotation.Async;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
 import java.util.Optional;
 
 @Log4j2
@@ -14,10 +16,12 @@ public abstract class BaseStrategy implements Strategy {
 
     protected final Symbol symbol;
     protected final DataPoller dataPoller;
+    protected final Deque<Candle> historicCandles;
 
     public BaseStrategy(final Symbol symbol, final DataPoller dataPoller) {
         this.symbol = symbol;
         this.dataPoller = dataPoller;
+        this.historicCandles = new ArrayDeque<>(getHistoricDataSize(symbol));
     }
 
     @Override
@@ -30,11 +34,19 @@ public abstract class BaseStrategy implements Strategy {
             final Candle candle = optionalCandle.get();
             log.info("Received candle: {}", candle);
             evaluate(candle);
+            addHistoricData(candle);
 
             optionalCandle = dataPoller.poll();
         }
 
         log.info("No more candles to process, exiting strategy for symbol: {}", symbol.getName());
+    }
+
+    private void addHistoricData(final Candle candle) {
+        if (!historicCandles.offer(candle)) {
+            historicCandles.poll();
+            historicCandles.offer(candle);
+        }
     }
 
     @Override
@@ -45,5 +57,15 @@ public abstract class BaseStrategy implements Strategy {
     @Override
     public void closePosition() {
 
+    }
+
+    private int getHistoricDataSize(final Symbol symbol) {
+        final Map<String, String> parameters = symbol.getParameters();
+
+        if (parameters.containsKey("historicDataSize")) {
+            return Integer.parseInt(parameters.get("historicDataSize"));
+        }
+
+        return 100;
     }
 }
